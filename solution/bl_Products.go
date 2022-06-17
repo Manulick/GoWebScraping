c2b37e55f3b8225cec1a9619cbcef637bc32038c
Emmanuel Nava Arévalo <52983654+Manulick@users.noreply.github.com>
2022-06-17 00:47:34 -0500

    add colly v2

--- solution/bl_Products.go
+++ solution/bl_Products.go
@@ -3,47 +3,50 @@ package main
 import (
 	"GoWebScraping/solution/openapi"
 	"fmt"
-	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/v2"
 	"log"
-	"sync"
 )
 
-var urlList = "https://laptops.mercadolibre.com.mx/laptops-accesorios/#menu=categories"
+var urlList = []string{
+	"https://laptops.mercadolibre.com.mx/laptops-accesorios/#menu=categories",
+	"https://listado.mercadolibre.com.mx/supermercado/bebidas/",
+	"https://listado.mercadolibre.com.mx/_Deal_deportes-y-fitness-accesorios",
+	"https://www.mercadolibre.com.mx/mas-vendidos/MLM1144",
+}
 
-func getUrlList(url string) interface{} {
+func getUrlList(urls []string) interface{} {
 	products := openapi.Products{}
 	product := openapi.Product{}
 
-	c := colly.NewCollector()
+	c := colly.NewCollector(
+		colly.Async(),
+	)
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("visiting ", r.URL)
 	})
-	c.OnHTML("ol.ui-search-layout", func(e *colly.HTMLElement) {
-		e.ForEach("a.ui-search-item__group__element", func(i int, e *colly.HTMLElement) {
-			link := e.Attr("href")
-
-			log.Println(link)
-
-			if i < 10 {
-
-				name, err := getData(link, "h1.ui-pdp-title")
-				price, err := getData(link, "span.andes-money-amount__fraction")
-				oldPrice, err := getData(link, "div.ui-pdp-products")
-				imgUrl := getDataAttribute(link, "img.ui-pdp-image.ui-pdp-gallery__figure__image", "data-src")
-				if err != nil {
-					log.Fatal(err)
-					return
-				}
-				product.Description = name
-				product.Price = price
-				product.OldPrice = oldPrice
-				product.ImageURL = imgUrl
-				products.Data = append(products.Data, product)
+	c.OnHTML("ol.ui-search-layout a.ui-search-item__group__element", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if len(products.Data) < 10 {
+			name, err := getData(link, "h1.ui-pdp-title")
+			price, err := getData(link, "span.andes-money-amount__fraction")
+			oldPrice, err := getData(link, "div.ui-pdp-products")
+			imgUrl := getDataAttribute(link, "img.ui-pdp-image.ui-pdp-gallery__figure__image", "data-src")
+			if err != nil {
+				log.Fatal(err)
+				return
 			}
-		})
+			product.Description = name
+			product.Price = price
+			product.OldPrice = oldPrice
+			product.ImageURL = imgUrl
+			products.Data = append(products.Data, product)
+		}
 	})
-	c.Visit(url)
+	for _, url := range urls {
+		c.Visit(url)
+	}
+	c.Wait()
 
 	return products
 }
@@ -51,11 +54,8 @@ func getUrlList(url string) interface{} {
 func getData(url string, element string) (string, error) {
 	var response string
 	var productError error
-	wg := sync.WaitGroup{}
-	wg.Add(1)
 	c := colly.NewCollector()
 
-	defer wg.Done()
 	c.OnHTML(element, func(e *colly.HTMLElement) {
 		attribute, err := e.DOM.Html()
 		if err != nil {
